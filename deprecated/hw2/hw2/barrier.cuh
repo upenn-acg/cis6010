82cb5733d7d55df69785a1e5e4f6f801eb485380
Joseph Devietti <devietti@cis.upenn.edu>
2025-09-17 10:14:11 -0400

    remove deprecated code

--- deprecated/hw2/hw2/barrier.cuh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-enum BarrierFlavor { KERNEL_LAUNCH_BARRIER, SPIN_BARRIER, TWO_LEVEL_BARRIER };
-
-extern void barrierTest(const BarrierFlavor);
-extern const char* barrierName(const BarrierFlavor);
